4c3f3c208ed2cefa0cd4851c118db3827fd4cb54
TRAVIS Eiler <eiler9233@students.cptc.edu>
2019-10-15 14:07:02 -0700

    Added Functionality

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,8 @@ var Student = /** @class */ (function () {
     }
     return Student;
 }());
+// Constants
+var programAttr = "data-program";
 // window.onload = function(){
 //
 // }
@@ -20,14 +22,38 @@ function processForm() {
     displayStudent(nextStu);
     clearForm(); // in preperation to enter next student
 }
-function displayStudent(s) {
+function displayStudent(stu) {
     // create LI element
     var newItem = document.createElement("li");
-    newItem.innerText = s.firstName + " " + s.lastName;
+    newItem.innerText = stu.firstName + " " + stu.lastName;
     var displaySection = document.querySelector("#student-list");
     var list = displaySection.querySelector("ul");
+    // embed student data in <li>, hide info with custom attributes, * attribute
+    newItem.setAttribute(programAttr, stu.program);
+    newItem.setAttribute("data-address", stu.address);
+    newItem.setAttribute("data-birthdate", stu.dateOfBirth.toString());
+    console.log(newItem);
+    // Need to create showStudentData method
+    // Call when <li> is clicked
+    newItem.onclick = showStudentData;
     // Add <li> as a child to <ul>
     list.appendChild(newItem);
+}
+/**
+ * Shows individual student data when a student <li> is clicked
+ */
+function showStudentData() {
+    //alert("You clicked an <li> block element");
+    //console.log(this);
+    var currListItem = this; // cast this to appropriate type HTMLLIElement
+    var name = currListItem.innerText;
+    var program = currListItem.getAttribute(programAttr);
+    // alert(name + " is studying " + program);
+    // how to grab h2 and display student name
+    document.querySelector("#display > h2").innerHTML = name;
+    // let h2 = document.querySelector("#display > h2");
+    // h2.innerHTML = name;
+    document.querySelector("#display > p").innerHTML = name + " is studying " + program;
 }
 function getStudentFromForm() {
     var tempStu = new Student();
--- typescripts/main.ts
+++ typescripts/main.ts
@@ -19,22 +19,25 @@ class Student{
      */
     program:string;
 
-    /*constructor(fName, lName){
+    /*
+    constructor(fName, lName){
         this.firstName = fName;
         this.lastName = lName;
     }*/
 }
 
+// Constants
+const programAttr = "data-program";
 
 // window.onload = function(){
 //
 // }
-
 window.onload = main;
 function main(){
     let regBtn = document.querySelector("button");
     regBtn.onclick = processForm; // () to call function, not needed to establish one
 }
+
 
 function processForm(){
     // assume data is valid for now
@@ -43,16 +46,48 @@ function processForm(){
     clearForm(); // in preperation to enter next student
 }
 
-function displayStudent(s:Student):void{ // void, no return type
+
+function displayStudent(stu:Student):void{ // void, no return type
     // create LI element
     let newItem = document.createElement("li");
-    newItem.innerText = s.firstName + " " + s.lastName;
+    newItem.innerText = stu.firstName + " " + stu.lastName;
     let displaySection = document.querySelector("#student-list");
     let list = displaySection.querySelector("ul");
     
+    // embed student data in <li>, hide info with custom attributes, * attribute
+    newItem.setAttribute(programAttr, stu.program);
+    newItem.setAttribute("data-address", stu.address);
+    newItem.setAttribute("data-birthdate", stu.dateOfBirth.toString());
+
+    console.log(newItem);
+
+    // Need to create showStudentData method
+    // Call when <li> is clicked
+    newItem.onclick = showStudentData;
+
     // Add <li> as a child to <ul>
     list.appendChild(newItem);
 }
+
+
+/**
+ * Shows individual student data when a student <li> is clicked
+ */
+function showStudentData(){
+    //alert("You clicked an <li> block element");
+    //console.log(this);
+    let currListItem = <HTMLLIElement>this;  // cast this to appropriate type HTMLLIElement
+    let name = currListItem.innerText;
+    let program = currListItem.getAttribute(programAttr);
+    // alert(name + " is studying " + program);
+
+    // how to grab h2 and display student name
+    document.querySelector("#display > h2").innerHTML = name;
+    // let h2 = document.querySelector("#display > h2");
+    // h2.innerHTML = name;
+    document.querySelector("#display > p").innerHTML = name + " is studying " + program;
+}
+
 
 function getStudentFromForm():Student{ // :Student is a return type
     let tempStu = new Student();
@@ -64,6 +99,7 @@ function getStudentFromForm():Student{ // :Student is a return type
     console.log(tempStu);
     return tempStu;
 }
+
 
 function getInputValue(id:string):string{
     let elem = <HTMLInputElement>document.getElementById(id);
